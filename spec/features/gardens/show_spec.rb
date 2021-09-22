0b09c991620f4051c9a3615d749d9ac32eda1732
Henry Schmid <henry.schmid1@gmail.com>
2021-09-22 10:28:50 -0600

    Update garden #plants method to only get when < 100 days

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -9,14 +9,16 @@ RSpec.describe 'gardens show' do
     @plant2 = @plot1.plants.create!(name: 'tomato', description: 'red', days_to_harvest: 20)
     @plant3 = @plot2.plants.create!(name: 'pepper', description: 'yellow', days_to_harvest: 10)
     @plant4 = Plant.create!(name: 'lily', description: 'flower', days_to_harvest: 50)
+    @plant5 = @plot2.plants.create!(name: 'tomatillo', description: 'yellow', days_to_harvest: 100)
     PlantPlot.create!(plant: @plant1, plot: @plot2)
     visit garden_path(@garden)
   end
 
-  it 'shows list of plants included in gardens plots' do
+  it 'shows list of plants included in gardens plots < 100 days to harvest' do
     expect(page).to have_content(@plant1.name, count: 1)
     expect(page).to have_content(@plant2.name)
     expect(page).to have_content(@plant3.name)
+    expect(page).to_not have_content(@plant4.name)
     expect(page).to_not have_content(@plant4.name)
   end
 end
--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -4,4 +4,23 @@ RSpec.describe Garden do
   describe 'relationships' do
     it { should have_many(:plots) }
   end
+
+  before :each do
+    @garden = Garden.create!(name: "Schmiddy's Botanicals", organic: true)
+    @plot1 = @garden.plots.create!(number: 42, size: 'Large', direction: 'Northeast')
+    @plot2 = @garden.plots.create!(number: 87, size: 'Medium', direction: 'Southwest')
+    @plant1 = @plot1.plants.create!(name: 'kale', description: 'green', days_to_harvest: 30)
+    @plant2 = @plot1.plants.create!(name: 'tomato', description: 'red', days_to_harvest: 20)
+    @plant3 = @plot2.plants.create!(name: 'pepper', description: 'yellow', days_to_harvest: 10)
+    @plant4 = Plant.create!(name: 'lily', description: 'flower', days_to_harvest: 150)
+    @plant5 = @plot2.plants.create!(name: 'tomatillo', description: 'yellow', days_to_harvest: 100)
+    PlantPlot.create!(plant: @plant1, plot: @plot2)
+  end
+
+  it 'gets distinct plants for a garden' do
+    expect(@garden.plants.count).to eq(3)
+    expect(@garden.plants[0].name).to eq(@plant1.name)
+    expect(@garden.plants[1].name).to eq(@plant3.name)
+    expect(@garden.plants[2].name).to eq(@plant2.name)
+  end
 end
